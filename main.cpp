d228d5557c0da2c8d12b47be39980af36f6bd862
Yi Loo <iyool81@hotmail.com>
2017-04-18 20:10:17 +0800

    expanded floor with snow collision

--- main.cpp
+++ main.cpp
@@ -44,8 +44,8 @@ int fall;
 float r = 0.0;
 float g = 1.0;
 float b = 0.0;
-float ground_points[21][21][3];
-float ground_colors[21][21][4];
+float ground_points[200][200][3];
+float ground_colors[200][200][4];
 float accum = -10.0;
 
 typedef struct {
@@ -102,12 +102,12 @@ namespace {
 	// received.
     void initParticles(int i) {
         par_sys[i].alive = true;
-        par_sys[i].life = 2.2;
-        par_sys[i].fade = float(rand()%100)/1000.0f+0.003f;
+        par_sys[i].life = 3.2;
+        par_sys[i].fade = float(rand()%10)/1000.0f+0.003f;
 
-        par_sys[i].xpos = (float) (rand() % 100) -50;
+        par_sys[i].xpos = (float) (rand() % 200) -100;
         par_sys[i].ypos = 100.0;
-        par_sys[i].zpos = (float) (rand() % 100) -50 ;
+        par_sys[i].zpos = (float) (rand() % 200) -100 ;
 
         par_sys[i].red = 0.5;
         par_sys[i].green = 0.5;
@@ -129,11 +129,11 @@ namespace {
 
       // Ground Verticies
         // Ground Colors
-        for (z = 0; z < 21; z++) {
-          for (x = 0; x < 21; x++) {
-            ground_points[x][z][0] = x - 10.0;
+        for (z = 0; z < 200; z++) {
+          for (x = 0; x < 200; x++) {
+            ground_points[x][z][0] = x - 100.0;
             ground_points[x][z][1] = accum;
-            ground_points[x][z][2] = z - 10.0;
+            ground_points[x][z][2] = z - 100.0;
 
             ground_colors[z][x][0] = r; // red value
             ground_colors[z][x][1] = g; // green value
@@ -271,14 +271,50 @@ namespace {
           par_sys[loop].life -= par_sys[loop].fade;
 
           if (par_sys[loop].ypos <= -10) {
-            int zi = z - zoom + 10;
-            int xi = x + 10;
-            ground_colors[zi][xi][0] = 1.0;
-            ground_colors[zi][xi][2] = 1.0;
-            ground_colors[zi][xi][3] += 1.0;
-            if (ground_colors[zi][xi][3] > 1.0) {
-              ground_points[xi][zi][1] += 0.1;
+			int zi = z + 100;
+			int xi = x + 100;
+            int range_zi_minus;
+            int range_zi_plus;
+            int range_xi_minus;
+			int range_xi_plus;
+            if (zi < 2){
+                range_zi_minus = 0;
+            }else{
+                range_zi_minus = zi - 3;
             }
+            if (199 - zi < 3){
+                range_zi_plus = 199;
+            }else{
+                range_zi_plus = zi + 3;
+            }
+            if (xi < 2){
+                range_xi_minus = 0;
+            }else{
+                range_xi_minus = xi - 3;
+            }
+            if (199 - xi < 3){
+                range_xi_plus = 199;
+            }else{
+                range_xi_plus = xi + 3;
+            }
+
+            // std::cout << zi<<" "<<xi << std::endl;
+            for(int zzi = range_zi_minus; zzi <=range_zi_plus; ++zzi){
+                for(int xxi =range_xi_minus; xxi <= range_xi_plus; ++xxi){
+                    ground_colors[zzi][xxi][0] = 1.0;
+                    ground_colors[zzi][xxi][2] = 1.0;
+                    ground_colors[zzi][xxi][3] += 1.0;
+                    if (ground_colors[zzi][xxi][3] > 1.0) {
+                      ground_points[xxi][zzi][1] += 0.1;
+                    }
+                }
+            }
+            // ground_colors[zi][xi][0] = 1.0;
+            // ground_colors[zi][xi][2] = 1.0;
+            // ground_colors[zi][xi][3] += 1.0;
+            // if (ground_colors[zi][xi][3] > 1.0) {
+            //   ground_points[xi][zi][1] += 0.1;
+            // }
             par_sys[loop].life = -1.0;
           }
 
@@ -436,6 +472,33 @@ namespace {
 		//drawSystem();
 		//x	cout << "bvh1";
 		drawSnow();
+
+        glColor3f(r, g, b);
+        glBegin(GL_QUADS);
+          // along z - y const
+          for (int i = -100; i+1 < 100; i++) {
+            // along x - y const
+            for (int j = -100; j+1 < 100; j++) {
+              glColor3fv(ground_colors[i+100][j+100]);
+              glVertex3f(ground_points[j+100][i+100][0],
+                    ground_points[j+100][i+100][1],
+                    ground_points[j+100][i+100][2] + zoom);
+              glColor3fv(ground_colors[i+100][j+1+100]);
+              glVertex3f(ground_points[j+1+100][i+100][0],
+                    ground_points[j+1+100][i+100][1],
+                    ground_points[j+1+100][i+100][2] + zoom);
+              glColor3fv(ground_colors[i+1+100][j+1+100]);
+              glVertex3f(ground_points[j+1+100][i+1+100][0],
+                    ground_points[j+1+100][i+1+100][1],
+                    ground_points[j+1+100][i+1+100][2] + zoom);
+              glColor3fv(ground_colors[i+1+100][j+100]);
+              glVertex3f(ground_points[j+100][i+1+100][0],
+                    ground_points[j+100][i+1+100][1],
+                    ground_points[j+100][i+1+100][2] + zoom);
+            }
+
+          }
+        glEnd();
 
 		// This draws the coordinate axes when you're rotating, to
 		// keep yourself oriented.
